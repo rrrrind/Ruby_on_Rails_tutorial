966c4a57cef28dd44c96b0681641c628d46f37f3
rrrrind <masaya.mori0905@gmail.com>
2021-06-18 21:40:45 +0900

    exercise 1

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
 
   def hello
-    render html: "hello world!"
+    render html: "Hello, world!"
+  end
+  def holamundo
+    render html: "¡Hola, mundo!"
+  end
+  def goodbye
+    render html: "Goodbye, world!"
   end
 end
