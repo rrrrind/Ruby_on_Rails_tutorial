f0515327d73ee9da090c9b333d6bfe3684afc182
rrrrind <masaya.mori0905@gmail.com>
2021-07-09 01:06:21 +0900

    Comp to 8.3

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -14,4 +14,11 @@ class User < ApplicationRecord
   # パスワードのハッシュ化(ハッシュ関数)
   has_secure_password
   validates(:password, {presence: true, length:{minimum:6}})
+
+  # 渡された文字列のハッシュ値を返す
+  #def User.digest(string)
+  #  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  #                                                BCrypt::Engine.cost
+  #  BCrypt::Password.create(string, cost: cost)
+  #end
 end
