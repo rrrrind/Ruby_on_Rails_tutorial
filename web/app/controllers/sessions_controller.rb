53e611a2f95899d8805e853a17834f568b7786a6
rrrrind <masaya.mori0905@gmail.com>
2021-07-20 11:52:36 +0900

    Fix dropdown-menu

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      #current_user
       redirect_to user
     else
       # エラーメッセージ(フラッシュメッセージ)
@@ -19,4 +20,5 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_path
   end
+  
 end
--- web/app/helpers/sessions_helper.rb
+++ web/app/helpers/sessions_helper.rb
@@ -8,11 +8,12 @@ module SessionsHelper
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user = User.find_by(id: session[:user_id]) if @current_user.nil?
     end
   end
 
   def logged_in?
+    current_user
     !@current_user.nil?
   end
 
